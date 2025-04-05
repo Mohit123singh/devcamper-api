d64866f2060b0cc3dc3b68ae6e14611cec4ebc55
Lenovo <mohiit.singh600@gmail.com>
2025-04-05 21:53:58 +0530

    Added-seeder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,39 +1,100 @@
+const asyncHandler=require('express-async-handler')
+const ErrorResponse=require('../utils/errorResponse')
+const Bootcamp=require('../models/Bootcamp')
+
 // @desc   Get all bootcamps
 // @route  GET /api/v1/bootcamps
 // @access Public
-const getBootcamps=(req,res,next)=>{
-    res.status(200).json({success:true,msg:'Show all bootcamps'})
-}
+const getBootcamps=asyncHandler(async(req,res,next)=>{
+    
+    const bootcamps=await Bootcamp.find()
+    res.status(200).json({
+        success:true,
+        count:bootcamps.length,
+        data:bootcamps,
+    })
+})
 
 // @desc   Get Single bootcamps
 // @route  GET /api/v1/bootcamps/:id
 // @access Public
-const getBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Show bootcamp ${req.params.id}`})
-}
+const getBootcamp=asyncHandler(async(req,res,next)=>{
+
+    const bootcamp=await Bootcamp.findById(req.params.id);
+        
+    if(!bootcamp)
+    {
+       return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`,404));
+    }
+      
+
+    res.status(200).json({
+        success:true,
+        data:bootcamp,
+    })
+
+})
+    
 
 
 // @desc   Create new bootcamp
 // @route  POST /api/v1/bootcamps
 // @access Private
-const createBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:'Create new bootcamp'})
-}
+const createBootcamp=asyncHandler(async(req,res,next)=>{
+
+    const bootcamp=await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success:true,
+            data:bootcamp
+        })
+
+    
+   
+})
 
 
 // @desc   Update bootcamp
 // @route  PUT /api/v1/bootcamps/:id
 // @access Private
-const updateBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Update bootcamp ${req.params.id}`})
-}
+const updateBootcamp=asyncHandler(async(req,res,next)=>{
+
+    const bootcamp=await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+    })
+    if(!bootcamp)
+    {
+       return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`,404));
+    }
+      
+
+    res.status(200).json({
+        success:true,
+        data:bootcamp,
+    })
+   
+})
 
 // @desc   Delete bootcamp
 // @route  DELETE /api/v1/bootcamps/:id
 // @access Private
-const deleteBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Delete bootcamp ${req.params.id}`})
-}
+const deleteBootcamp=asyncHandler(async(req,res,next)=>{
+   
+
+    const bootcamp=await Bootcamp.findByIdAndDelete(req.params.id)
+    if(!bootcamp)
+    {
+        return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`,404));
+    }
+      
+
+    res.status(200).json({
+        success:true,
+        data:{},
+    })
+   
+});
 
 
 module.exports={
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express=require('express');
 const dotenv=require('dotenv')
 const morgan=require('morgan')
+const colors=require('colors')
+const connectDB=require('./config/db')
+const notFound=require('./middleware/notFound')
+const errorHandler=require('./middleware/error')
 
-// Route Files
-const bootcamps=require('./routes/bootcamps');
+
 
 //load env vars
 dotenv.config({path:'./config/config.env'});
 
+// connect to database
+connectDB();
+
+// Route Files
+const bootcamps=require('./routes/bootcamps');
+
 const app=express();
 
+// body parser:
+app.use(express.json());
 
 // Dev logging middleware
 if(process.env.NODE_ENV==='development')
@@ -18,11 +29,18 @@ if(process.env.NODE_ENV==='development')
 }
 
 
+
+
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps);
+
+app.use(notFound)
+app.use(errorHandler);
+
 
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold);
 })
+
